1dfee5898d4e6740277151f617f906221d059ff1
Dario Di Nucci <linkin88mail@gmail.com>
2017-03-07 09:44:57 +0100

    Move to github.io

---
+++ js/script.js
@@ -0,0 +1,34 @@
+$(document).ready(function () {
+    $("#menu").load("menu.html", function () {
+        if (document.location.href.indexOf("index.html") > -1) {
+            $("#homeLink").addClass("activeMenuLink");
+        } else if (document.location.href.indexOf("research.html") > -1) {
+            $("#researchLink").addClass("activeMenuLink");
+        } else if (document.location.href.indexOf("publications.html") > -1) {
+            $("#publicationsLink").addClass("activeMenuLink");
+        } else if (document.location.href.indexOf("teaching.html") > -1) {
+            $("#teachingLink").addClass("activeMenuLink");
+            $('#thesis').DataTable({
+                "info": false,
+                "lengthChange": false,
+                "paging": false,
+                "searching": true
+            });
+            $('#courses').DataTable({
+                "info": false,
+                "lengthChange": false,
+                "paging": false,
+                "searching": true
+            });
+        } else if (document.location.href.indexOf("projects.html") > -1) {
+            $("#projectsLink").addClass("activeMenuLink");
+        } else if (document.location.href.indexOf("services.html") > -1) {
+            $("#servicesLink").addClass("activeMenuLink");
+        } else if (document.location.href.indexOf("awards.html") > -1) {
+            $("#awardsLink").addClass("activeMenuLink");
+        } else {
+            $("#homeLink").addClass("activeMenuLink");
+        }
+    });
+    $("#footer").load("footer.html");
+});
