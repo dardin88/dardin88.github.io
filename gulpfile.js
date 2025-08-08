cf7593e50852c1247780ca51f54901c46797cf25
Dario Di Nucci <d.dinucci@uvt.nl>
2025-08-08 18:46:04 +0200

    Add modern interactions and animations for improved user experience
    - Implemented smooth scroll behavior for internal links and mobile menu collapse on navigation clicks.
    - Added support for reduced motion preferences to enhance accessibility.
    - Introduced Intersection Observer for fade-in animations on elements.
    - Simplified hover effects and removed parallax effect to reduce flickering and improve performance.
    - Added loading animations for external links and improved mobile menu interactions.
    - Included slide animations for navbar toggling.

--- gulpfile.js
+++ gulpfile.js
@@ -1,21 +1,24 @@
 const gulp = require('gulp');
 const rename = require('gulp-rename');
-const uglify = require('gulp-uglify');
+const terser = require('gulp-terser');
 const cleanCSS = require('gulp-clean-css');
+const sourcemaps = require('gulp-sourcemaps');
+const postcss = require('gulp-postcss');
+const autoprefixer = require('autoprefixer');
 
 // Minify CSS
 gulp.task('css:minify', function() {
   return gulp.src([
       './css/*.css',
       '!./css/*.min.css'
-    ])
+    ], { allowEmpty: true })
+    .pipe(sourcemaps.init())
+    .pipe(postcss([autoprefixer()]))
     .pipe(cleanCSS({
-      level: 2,
-      compatibility: 'ie8'
+      level: 2
     }))
-    .pipe(rename({
-      suffix: '.min'
-    }))
+    .pipe(rename({ suffix: '.min' }))
+    .pipe(sourcemaps.write('.'))
     .pipe(gulp.dest('./css'));
 });
 
@@ -24,13 +27,18 @@ gulp.task('js:minify', function() {
   return gulp.src([
       './js/*.js',
       '!./js/*.min.js'
-    ])
-    .pipe(uglify())
-    .pipe(rename({
-      suffix: '.min'
-    }))
+    ], { allowEmpty: true })
+    .pipe(sourcemaps.init())
+    .pipe(terser())
+    .pipe(rename({ suffix: '.min' }))
+    .pipe(sourcemaps.write('.'))
     .pipe(gulp.dest('./js'));
 });
 
 // Default task
+gulp.task('watch', function() {
+  gulp.watch(['./css/*.css', '!./css/*.min.css'], gulp.series('css:minify'));
+  gulp.watch(['./js/*.js', '!./js/*.min.js'], gulp.series('js:minify'));
+});
+
 gulp.task('default', gulp.series('css:minify', 'js:minify'));
